61ea2c78d4003a8fd315ea61cfbde8476ab0ca76
ianscottmartin <ismartin41@gmail.com>
2023-06-04 19:23:11 -0600

    losads

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  {useEffect}
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
